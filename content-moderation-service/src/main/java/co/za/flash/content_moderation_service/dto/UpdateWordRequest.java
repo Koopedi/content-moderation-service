737cecea2c6da94ac9c6b31a95e6fab47ad6bb3c
Koopedi <36690573+Koopedi@users.noreply.github.com>
2026-02-22 22:06:13 +0200

    changed db to mssql

--- content-moderation-service/src/main/java/co/za/flash/content_moderation_service/dto/UpdateWordRequest.java
+++ content-moderation-service/src/main/java/co/za/flash/content_moderation_service/dto/UpdateWordRequest.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UpdateWordRequest {
     @NotBlank(message = "Word cannot be blank")
     @Size(max = 100, message = "Word cannot exceed 100 characters")
